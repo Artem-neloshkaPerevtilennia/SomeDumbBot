09196db326cf5ba94bef6c4fb3cb581acd5ad1d3
Artem Bandura <153463378+Artem-neloshka@users.noreply.github.com>
2024-07-07 13:46:39 +0300

    a little functional and changed tub to 2-spaces

--- Program.cs
+++ Program.cs
@@ -1,13 +1,31 @@
-﻿using System;
-using Telegram.Bot;
-using Telegram.Bot.Args;
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
 
-namespace WeatherBot{
-    class Program{
-        static void Main(string[] args)
-        {
-            var bot = new TelegramBotClient("7012783078:AAH3CIDoJ7mvfWr2TCMsz2W6XeKcg3ZKgts");
-            Console.ReadLine();
-        }
+namespace WeatherBot
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      var bot = new TelegramBotClient("7012783078:AAH3CIDoJ7mvfWr2TCMsz2W6XeKcg3ZKgts");
+      bot.StartReceiving(Update, Error);
+      Console.ReadLine();
     }
+
+    private static async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+    {
+      var message = update.Message;
+      if (message.Text == null) return;
+      if (message.Text.Contains("hui"))
+      {
+        await botClient.SendTextMessageAsync(message.Chat.Id, "сам такий");
+        return;
+      }
+    }
+
+    private static async Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
